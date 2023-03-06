12468fdbc76594b095d3f3ffdae19381a641763f
Laurynas <laurynas1narbutas@gmail.com>
2023-03-06 14:56:31 +0200

    Add Film filtering, sorting and printing lambdas. Update .gitignore file

--- src/App.java
+++ src/App.java
@@ -1,26 +1,92 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 public class App {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+
+        Consumer<String> printToSout = System.out::println;
+        Consumer<String> printToFile = App::printToFile;
+        Consumer<String> printToSoutAndFile = x -> {
+            System.out.println(x);
+            printToFile(x);
+        };
+
         List<Filmas> filmai = List.of(
-                new Filmas("The Shawshank Redemption",9.2f,"Drama"),
-                new Filmas("The Godfather",9.2f,"Crime"),
-                new Filmas("The Dark Knight",9.0f,"Action"),
-                new Filmas("The Godfather Part II",9.0f,"Crime"),
-                new Filmas("12 Angry Men",9.0f,"Drama"),
-                new Filmas("Schindlers List",8.9f,"Drama"),
-                new Filmas("Naujas filmas",8.9f,"Comedy")
+                new Filmas("The Shawshank Redemption", 9.2f, "Drama", 90),
+                new Filmas("The Godfather", 9.2f, "Crime", 80),
+                new Filmas("The Dark Knight", 9.0f, "Action", 100),
+                new Filmas("The Godfather Part II", 9.0f, "Crime", 20),
+                new Filmas("12 Angry Men", 9.0f, "Drama", 30),
+                new Filmas("Schindlers List", 8.9f, "Drama", 59),
+                new Filmas("Naujas filmas", 8.9f, "Comedy", 200)
         );
 
-        var filmaiPagalZanra = filmai.stream()
-                .collect(Collectors.groupingBy(
-                        Filmas::getZanras,
-                        Collectors.mapping(Filmas::getNameRatingString,
-                                Collectors.toSet())
-                ));
+        System.out.print("""
+                Pagal ka norite filmus filtruoti:
+                1 > trumpus filmus
+                2 > geriausiai ivertintus
+                3 > dramas
+                """);
+        var filtras = switch (in.nextLine()) {
+            case "1" -> Filmas.isShortFilm;
+            case "2" -> Filmas.hasHighRating;
+            case "3" -> Filmas.isDrama;
+            default -> Filmas.defaultFilter;
+        };
 
-        System.out.println(filmaiPagalZanra);
+        System.out.print("""
+                Pagal ka norite filmus surusiuoti:
+                1 > pagal pavadinima
+                2 > pagal ivertinima
+                3 > pagal trukme
+                4 > pagal pavadinima[atvirksciai]
+                5 > pagal ivertinima[atvirksciai]
+                6 > pagal trukme[atvirksciai]
+                """);
+        var rusiavimas = switch (in.nextLine()) {
+            case "2" -> Filmas.byRating;
+            case "3" -> Filmas.byLength;
+            case "4" -> Filmas.byNameDesc;
+            case "5" -> Filmas.byRatingDesc;
+            case "6" -> Filmas.byLengthDesc;
+            default -> Filmas.byName;
+        };
+
+        System.out.print("""
+                Kur norite isvesti rezultata:
+                1 > i ekrana
+                2 > i faila
+                3 > i ekrana ir i faila
+                """);
+        var isvedimas = switch (in.nextLine()) {
+            case "2" -> printToFile;
+            case "3" -> printToSoutAndFile;
+            default -> printToSout;
+        };
+
+
+        var filmaiString = filmai.stream()
+                .filter(filtras)
+                .sorted(rusiavimas)
+                .map(filmas -> filmas.toString().concat(System.lineSeparator()))
+                .reduce("", String::concat);
+
+        isvedimas.accept(filmaiString);
+    }
+
+    private static void printToFile(String x) {
+        try {
+            Files.writeString(new File("filmas.txt").toPath(),
+                    x, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            System.err.println(e);
+            System.err.println("Ivyko failo rasymo klaida!");
+        }
     }
 }
--- src/Filmas.java
+++ src/Filmas.java
@@ -1,14 +1,36 @@
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Predicate;
+
 public class Filmas {
     private String pavadinimas;
     private float ivertinimas;
     private String zanras;
+    private int trukme;
 
-    public Filmas(String pavadinimas, float ivertinimas, String zanras) {
+    /* PREDICATES */
+    public static final Predicate<Filmas> defaultFilter = x -> true;
+    public static final Predicate<Filmas> isShortFilm = x -> x.trukme < 60;
+    public static final Predicate<Filmas> hasHighRating = x -> x.ivertinimas >= 9.0;
+    public static final Predicate<Filmas> isDrama = x -> x.zanras.equalsIgnoreCase("drama");
+
+    /* COMPARATORS */
+    public static final Comparator<Filmas> byRating = Comparator.comparingDouble(Filmas::getIvertinimas);
+    public static final Comparator<Filmas> byRatingDesc = Comparator.comparingDouble(Filmas::getIvertinimas).reversed();
+    public static final Comparator<Filmas> byLength = Comparator.comparingInt(Filmas::getTrukme);
+    public static final Comparator<Filmas> byLengthDesc = Comparator.comparingInt(Filmas::getTrukme).reversed();
+    public static final Comparator<Filmas> byName = Comparator.comparing(Filmas::getPavadinimas);
+    public static final Comparator<Filmas> byNameDesc = Comparator.comparing(Filmas::getPavadinimas).reversed();
+
+    public Filmas(String pavadinimas, float ivertinimas, String zanras, int trukme) {
         this.pavadinimas = pavadinimas;
         this.ivertinimas = ivertinimas;
         this.zanras = zanras;
+        this.trukme = trukme;
     }
 
+
+    // <editor-fold defaultstate="collapsed" desc="Getters/Setters">
     public String getPavadinimas() {
         return pavadinimas;
     }
@@ -33,6 +55,16 @@ public class Filmas {
         this.zanras = zanras;
     }
 
+    public int getTrukme() {
+        return trukme;
+    }
+
+    public void setTrukme(int trukme) {
+        this.trukme = trukme;
+    }
+
+    // </editor-fold>
+
     public String getNameRatingString() {
         return pavadinimas + " : " + ivertinimas + "*";
     }
@@ -43,6 +75,7 @@ public class Filmas {
                 "pavadinimas='" + pavadinimas + '\'' +
                 ", ivertinimas=" + ivertinimas +
                 ", zanras='" + zanras + '\'' +
+                ", trukme=" + trukme +
                 '}';
     }
 }
